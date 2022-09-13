910acc26f17b6691f94a3215749bb5a16025cea6
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-09-13 23:31:05 +0700

    Remove unused imports

--- Task-1.1.1/src/test/java/io/github/kapkekes/HeapSortTest.java
+++ Task-1.1.1/src/test/java/io/github/kapkekes/HeapSortTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class HeapSortTest {
