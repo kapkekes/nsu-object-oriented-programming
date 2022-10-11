a239fc7e3bb9b8418d6556128038680986c1be77
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-10-11 14:08:11 +0700

    Add several more Javadoc comments and functions
    - Add several functions with Javadoc comments
    - Add Javadoc comments to remaining functions
    - Add exception descriptions

--- Task_1_2_2/src/main/java/io/github/kapkekes/Tree.java
+++ Task_1_2_2/src/main/java/io/github/kapkekes/Tree.java
@@ -44,7 +44,7 @@ public class Tree<E> implements Iterable<E> {
     }
 
     private E value;
-    private List<Tree<E>> children;
+    private final List<Tree<E>> children;
     private Search mode;
     private int iteratorCount;
 
@@ -78,8 +78,13 @@ public class Tree<E> implements Iterable<E> {
      * Create an empty tree with the given value.
      *
      * @param val the value to set
+     * @throws NullPointerException if {@code val} is {@code null}
      */
-    public Tree(E val) {
+    public Tree(E val) throws NullPointerException {
+        if (val == null) {
+            throw new NullPointerException("can't create a tree with null value");
+        }
+
         value = val;
         children = new ArrayList<>();
         mode = Search.BREADTH;
@@ -106,6 +111,13 @@ public class Tree<E> implements Iterable<E> {
         return children.stream().mapToInt(Tree::size).reduce(Integer::sum).orElse(1);
     }
 
+    /**
+     * Returns an iterator over the elements in this tree in proper sequence. Order of the
+     * elements will be defined by {@code mode} field; use {@code with()} method to specify needed
+     * traverse option.
+     *
+     * @return an iterator over the elements
+     */
     @Override
     public Iterator<E> iterator() {
         List<Tree<E>> descendants =
@@ -121,37 +133,100 @@ public class Tree<E> implements Iterable<E> {
         return new TreeIterator<>(descendants);
     }
 
+    /**
+     * Add a new value to the tree as a new branch.
+     *
+     * @param val a value to add
+     * @return {@code this}
+     * @throws ConcurrentModificationException if this tree has active iterators at the moment
+     * @throws NullPointerException if {@code val} is {@code null}
+     */
     public Tree<E> add(E val) throws ConcurrentModificationException, NullPointerException {
         if (val == null) {
-            throw new NullPointerException();
+            throw new NullPointerException("can't add a null value to a tree");
         }
 
         if (iteratorCount > 0) {
-            throw new ConcurrentModificationException();
+            throw new ConcurrentModificationException(
+                    "can't add a value to a tree, as it has %d active iterators"
+                            .formatted(this.iteratorCount)
+            );
         }
 
         children.add(new Tree<>(val));
         return this;
     }
 
+    /**
+     * Add an existing tree as a branch to this tree.
+     *
+     * @param branch a tree to add
+     * @return {@code this}
+     * @throws ConcurrentModificationException if this tree has active iterators at the moment
+     * @throws NullPointerException if {@code branch} is {@code null}
+     */
     public Tree<E> add(Tree<E> branch)
             throws ConcurrentModificationException, NullPointerException {
         if (branch == null) {
-            throw new NullPointerException();
+            throw new NullPointerException("can't add a null branch to a tree");
         }
 
         if (iteratorCount > 0) {
-            throw new ConcurrentModificationException();
+            throw new ConcurrentModificationException(
+                    "can't add a branch to a tree, as it has %d active iterators"
+                            .formatted(this.iteratorCount)
+            );
         }
 
         children.add(branch);
         return this;
     }
 
+    /**
+     * Get the value in this node.
+     *
+     * @return the value in this node
+     */
+    public E get() {
+        return this.value;
+    }
+
+    /**
+     * Get a branch of this tree.
+     *
+     * @param index the number of a needed branch
+     * @return a requested branch
+     * @throws IndexOutOfBoundsException if the index is out of range {@code index < 0 || index >=
+     *     children.size()}
+     */
     public Tree<E> get(int index) throws IndexOutOfBoundsException {
         return children.get(index);
     }
 
+    /**
+     * Set the value in this node.
+     *
+     * @param val value to set
+     * @return {@code this}
+     * @throws NullPointerException if {@code val} is {@code null}
+     */
+    public Tree<E> set(E val) throws NullPointerException {
+        if (val == null) {
+            throw new NullPointerException("can't set a null value as a tree value");
+        }
+
+        value = val;
+        return this;
+    }
+
+    /**
+     * Remove a branch from this tree.
+     *
+     * @param index the number of a branch to remove
+     * @return a removed branch
+     * @throws IndexOutOfBoundsException if the index is out of range {@code index < 0 || index >=
+     *     children.size()}
+     */
     public Tree<E> remove(int index) throws IndexOutOfBoundsException {
         return children.remove(index);
     }
