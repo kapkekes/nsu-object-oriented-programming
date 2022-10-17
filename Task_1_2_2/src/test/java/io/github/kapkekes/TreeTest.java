1c6f6e50a2639d3e42c99d0c72c1c2365de85e3a
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-10-17 09:46:06 +0700

    Add changes required by the teacher

---
+++ Task_1_2_2/src/test/java/io/github/kapkekes/TreeTest.java
@@ -0,0 +1,11 @@
+package io.github.kapkekes;
+
+import org.junit.jupiter.api.Test;
+
+/** A set of tests for the {@code Tree} class. */
+public class TreeTest {
+    @Test
+    public void SimpleTest() {
+
+    }
+}
