2446698bce16392667bcb61aa91345b3a3badb88
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-10-31 11:57:33 +0700

    Redefine Edge interface

--- Task_1_2_3/src/main/java/io/github/kapkekes/Edge.java
+++ Task_1_2_3/src/main/java/io/github/kapkekes/Edge.java
@@ -1,8 +1,22 @@
 package io.github.kapkekes;
 
+/**
+ * Interface, which defines an edge for usage with the graph and the vertex interfaces.
+ *
+ * @param <V> type of the name in the vertices
+ */
 public interface Edge<V> {
-    public double getWeight();
-    public Vertex<V> getFromVertex();
-    public Vertex<V> getToVertex();
-    public Edge<V> setWeight();
+    /**
+     * Get the first vertex of the edge.
+     *
+     * @return the first vertex
+     */
+    public Vertex<V> getVertexA();
+
+    /**
+     * Get the second vertex of the edge.
+     *
+     * @return the second vertex
+     */
+    public Vertex<V> getVertexB();
 }
