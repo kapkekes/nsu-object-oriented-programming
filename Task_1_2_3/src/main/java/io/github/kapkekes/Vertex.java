39183569718aeb3bf48de62975511674ba7964fa
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-10-31 11:18:48 +0700

    Update Vertex interface

--- Task_1_2_3/src/main/java/io/github/kapkekes/Vertex.java
+++ Task_1_2_3/src/main/java/io/github/kapkekes/Vertex.java
@@ -1,6 +1,23 @@
 package io.github.kapkekes;
 
+/**
+ * Interface, which defines a vertex for usage with the graph and the edge interfaces.
+ *
+ * @param <V> type of the name in the vertex
+ */
 public interface Vertex<V> {
+    /**
+     * Get name of the vertex.
+     *
+     * @return name of the vertex
+     */
     public V getName();
-    public Vertex<V> setName();
+
+    /**
+     * Set a new name for the vertex.
+     *
+     * @param name new name
+     * @return {@code this}
+     */
+    public Vertex<V> setName(V name);
 }
