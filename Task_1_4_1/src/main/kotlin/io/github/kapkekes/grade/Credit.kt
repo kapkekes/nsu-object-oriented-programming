e0a38de03f0dc5bffccb290568e2f121dee29b5e
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-12-07 11:58:50 +0700

    Change grades back to enums

--- Task_1_4_1/src/main/kotlin/io/github/kapkekes/grade/Credit.kt
+++ Task_1_4_1/src/main/kotlin/io/github/kapkekes/grade/Credit.kt
@@ -1,9 +1,8 @@
 package io.github.kapkekes.grade
 
 /** Provides possible grades for credits. */
-sealed class Credit(
+enum class Credit(
     override val points: Int
 ): Grade {
-    object FAIL : Credit(0)
-    object PASS : Credit(5)
+    FAIL(0), PASS(5)
 }
