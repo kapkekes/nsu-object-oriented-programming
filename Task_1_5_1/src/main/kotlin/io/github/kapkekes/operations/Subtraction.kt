ee0601f7f8b6e5782cc6229be411ff773cd9ad30
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-12-13 18:26:01 +0700

    Add basic arithmetic operations

---
+++ Task_1_5_1/src/main/kotlin/io/github/kapkekes/operations/Subtraction.kt
@@ -0,0 +1,10 @@
+package io.github.kapkekes.operations
+
+import io.github.kapkekes.Action
+import io.github.kapkekes.Operation
+
+object Subtraction: Operation {
+    override val token: String = "-"
+    override val arity: Int = 2
+    override val action: Action = { pair -> pair.component1() - pair.component2() }
+}
