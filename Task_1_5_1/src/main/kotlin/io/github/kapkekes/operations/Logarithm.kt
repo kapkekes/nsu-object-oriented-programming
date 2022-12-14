121294be74b86676224d7e3d4f2d7d4cf9fbda81
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-12-15 00:18:06 +0700

    Restructure operations

--- Task_1_5_1/src/main/kotlin/io/github/kapkekes/operations/Logarithm.kt
+++ Task_1_5_1/src/main/kotlin/io/github/kapkekes/operations/Logarithm.kt
@@ -1,10 +1,10 @@
 package io.github.kapkekes.operations
 
-import io.github.kapkekes.Action
-import io.github.kapkekes.Operation
-import io.github.kapkekes.log
+import io.github.kapkekes.calculator.Action
+import io.github.kapkekes.calculator.Operation
+import io.github.kapkekes.complex.log
 
-object Logarithm: Operation {
+class Logarithm: Operation {
     override val token: String = "log"
     override val arity: Int = 2
     override val action: Action = { pair -> log(pair.component2(), pair.component1()) }
--- Task_1_5_1/src/main/kotlin/io/github/kapkekes/operations/Power.kt
+++ Task_1_5_1/src/main/kotlin/io/github/kapkekes/operations/Power.kt
@@ -1,9 +1,9 @@
 package io.github.kapkekes.operations
 
-import io.github.kapkekes.Action
-import io.github.kapkekes.Operation
+import io.github.kapkekes.calculator.Action
+import io.github.kapkekes.calculator.Operation
 
-object Power: Operation {
+class Power: Operation {
     override val token: String = "pow"
     override val arity: Int = 2
     override val action: Action = { pair -> pair.component1().pow(pair.component2()) }
