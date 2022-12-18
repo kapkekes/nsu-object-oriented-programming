07558355a47972e6ad3c6d23c115812e05d2d71f
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-12-19 01:04:21 +0700

    Update CreditBook.kt

--- Task_1_4_1/src/main/kotlin/io/github/kapkekes/CreditBook.kt
+++ Task_1_4_1/src/main/kotlin/io/github/kapkekes/CreditBook.kt
@@ -1,61 +1,88 @@
 package io.github.kapkekes
 
 import io.github.kapkekes.grade.Grade
+import kotlin.jvm.Throws
 
 
-class CreditBook(
-    val student: Person,
-    val id: Int,
-    val speciality: String,
-    termsQuantity: Int = 8,
-) {
+/** Represents a simple credit book. */
+class CreditBook(termsQuantity: Int = 8) {
     init { // validate primary constructor
-        require(termsQuantity > 0) {
-            "[termsQuantity] should be positive"
-        }
+        require(termsQuantity > 0) { "[termsQuantity] should be positive" }
     }
-
-    var diplomaGrade: Grade? = null
-    val honorPossibility: Boolean
-        get() {
-            val supplementReq = subjectEntries.map { (name, entries) ->
-                pages[entries.max()][name]!!.grade?.points ?: 5
-            } .count { it == 5 } > subjectEntries.count() * 0.75
-
-            val bookReq = pages.all { page ->
-                page.values.mapNotNull { subject ->
-                    subject.grade?.points
-                } .all { it > 5 }
-            }
-
-            val diplomaReq = (diplomaGrade?.points ?: 5) == 5
-
-            return supplementReq && bookReq && diplomaReq
-        }
 
     private val pages: List<MutableMap<String, Subject>> = List(termsQuantity) { HashMap() }
     private val subjectEntries: MutableMap<String, MutableSet<Int>> = HashMap()
 
-    fun calculateGPA(): Double {
+    /** Current diploma grade, set to null by default. */
+    var diplomaGrade: Grade? = null
+        set(value) {
+            field = value ?: throw NullPointerException("Can't set diplomaGrade to null")
+        }
+
+    /** Returns the [Boolean] value depending on if the student can get a diploma with honour. */
+    val canGetHonoured: Boolean
+        get() {
+            val supplementReq = subjectEntries.map { (name, entries) ->
+                pages[entries.max()][name]!!.grade.points
+            } .count { it == 5 } > (subjectEntries.count() * 0.75)
+
+            val bookReq = pages.all { page ->
+                page.values.map { subject ->
+                    subject.grade.points
+                } .all { it >= 4 }
+            }
+
+            val diplomaReq = diplomaGrade?.points == 5
+
+            return supplementReq && bookReq && diplomaReq
+        }
+
+    /** Returns the [Boolean] value depending on if the student gets an increased stipend in this semester. */
+    val isStipendIncreased: Boolean
+        get() {
+            val current = getCurrentTerm()
+            if (current == 1) {
+                return false
+            }
+
+            return pages[current - 1 - 1].values.all { it.grade.points == 5 }
+        }
+
+    /** Returns the grade point average. */
+    fun getGPA(): Double {
         return pages.flatMap { page: Map<String, Subject> ->
-            page.values.mapNotNull { field: Subject ->
-                field.grade?.points
+            page.values.map { field: Subject ->
+                field.grade.points
             }
         } .average()
     }
 
-    fun calculateDiplomaAverage(): Double {
-        return subjectEntries.mapNotNull { (name, entries) ->
-            pages[entries.max()][name]!!.grade?.points
+    /** Returns the diploma average grade. */
+    fun getDiplomaAverage(): Double {
+        return subjectEntries.map { (name, entries) ->
+            pages[entries.max()][name]!!.grade.points
         } .average()
     }
 
-    fun addSubject(semester: Int, name: String, lecturer: Person): CreditBook {
-        require(semester in 0..pages.size) {
-            "this book consists from ${pages.size + 1} semesters, can't find semester #$semester"
+    /** Returns the current term number. */
+    fun getCurrentTerm(): Int {
+        return subjectEntries.map { (_, entries) -> entries.max() } .max() + 1
+    }
+
+    /**
+     * Tries to add a [name] subject of [lecturer] with [grade] to the [semester].
+     *
+     * Throws [IllegalArgumentException] if the [name] subject already exists or the [semester] is out of range.
+     */
+    @Throws(IllegalArgumentException::class)
+    fun addSubject(semester: Int, name: String, lecturer: Person, grade: Grade): CreditBook {
+        require(semester in 1..pages.size) {
+            "this book consists from ${pages.size} semesters, can't find semester #$semester"
         }
 
-        require(name !in pages[semester].keys) {
+        val real = semester - 1
+
+        require(name !in pages[real].keys) {
             "$name already exists in semester #$semester"
         }
 
@@ -63,19 +90,21 @@ class CreditBook(
             subjectEntries[name] = HashSet()
         }
 
-        subjectEntries[name]!!.add(semester)
-        pages[semester][name] = Subject(name, lecturer)
+        subjectEntries[name]!!.add(real)
+        pages[real][name] = Subject(name, lecturer, grade)
 
         return this
     }
 
+    /** Returns the [name] subject from [semester] if it exists, null otherwise. */
     fun getSubject(semester: Int, name: String): Subject? {
-        if (semester !in pages.indices) {
+        if (semester - 1 !in pages.indices) {
             return null
         }
 
-        return pages[semester][name]
+        return pages[semester - 1][name]
     }
 
-    data class Subject(val name: String, val lecturer: Person, var grade: Grade? = null)
+    /** Represents a [Subject] field in [CreditBook]. */
+    data class Subject(val name: String, val lecturer: Person, val grade: Grade)
 }
