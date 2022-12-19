84f8c4b3b87f62a995574c2cfda91c2cd2fecd6f
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-12-20 01:54:51 +0700

    Add documentation framework for Kotlin

--- Task_1_4_1/build.gradle.kts
+++ Task_1_4_1/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.8.0-Beta"
     id("jacoco")
+    id("org.jetbrains.dokka") version "1.7.20"
 }
 
 group = "io.github.kapkekes"
