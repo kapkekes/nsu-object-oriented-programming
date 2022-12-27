7c99860973910f10fcf7a0c218c3cf87daea9b16
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-12-28 01:39:21 +0700

    Fix style and imports

--- Task_1_5_1/src/main/kotlin/io/github/kapkekes/operations/logic/False.kt
+++ Task_1_5_1/src/main/kotlin/io/github/kapkekes/operations/logic/False.kt
@@ -1,8 +1,8 @@
 package io.github.kapkekes.operations.logic
 
 import io.github.kapkekes.calculator.Action
-import io.github.kapkekes.complex.Complex
 import io.github.kapkekes.calculator.Operation
+import io.github.kapkekes.complex.Complex
 
 /** False representation as a [Complex]. */
 object False : Operation {
