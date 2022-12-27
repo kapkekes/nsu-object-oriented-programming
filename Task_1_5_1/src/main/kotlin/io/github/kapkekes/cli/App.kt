1cbe302dc3b92461b872affee6347abe112b65fd
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-12-28 00:59:30 +0700

    Implement CLI

--- Task_1_5_1/src/main/kotlin/io/github/kapkekes/cli/App.kt
+++ Task_1_5_1/src/main/kotlin/io/github/kapkekes/cli/App.kt
@@ -1,4 +1,48 @@
 package io.github.kapkekes.cli
 
+import io.github.kapkekes.Calculator
+import io.github.kapkekes.Operation
+import io.github.kapkekes.operations.basic.Addition
+import io.github.kapkekes.operations.basic.Division
+import io.github.kapkekes.operations.basic.Logarithm
+import io.github.kapkekes.operations.basic.Multiplication
+import io.github.kapkekes.operations.basic.Power
+import io.github.kapkekes.operations.basic.SquareRoot
+import io.github.kapkekes.operations.basic.Subtraction
+import io.github.kapkekes.operations.flow.Exit
+import io.github.kapkekes.operations.logic.Equality
+import io.github.kapkekes.operations.logic.False
+import io.github.kapkekes.operations.logic.Greater
+import io.github.kapkekes.operations.logic.GreaterOrEqual
+import io.github.kapkekes.operations.logic.IfElse
+import io.github.kapkekes.operations.logic.Inequality
+import io.github.kapkekes.operations.logic.Less
+import io.github.kapkekes.operations.logic.LessOrEqual
+import io.github.kapkekes.operations.logic.True
+import io.github.kapkekes.operations.trigonometry.Cosine
+import io.github.kapkekes.operations.trigonometry.Sine
+
+/** Main routine of the application. */
 fun main() {
+    val operations: Set<Operation> = setOf(
+        Addition, Division, Logarithm, Multiplication, Power, SquareRoot, Subtraction,
+        Exit,
+        Equality, False, Greater, GreaterOrEqual, IfElse, Inequality, Less, LessOrEqual, True,
+        Cosine, Sine
+    )
+
+    val calculator = Calculator(operations)
+
+    routine@while (true) {
+        try {
+            println(calculator.evaluate(readln()))
+        } catch (e: ArithmeticException) {
+            println(e.message)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        } catch (e: RuntimeException) {
+            println(e.message)
+            break@routine
+        }
+    }
 }
