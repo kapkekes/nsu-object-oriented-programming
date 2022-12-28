6ef126289c208394f353e08b1310f961e7ced4f3
kapkekes <86285206+kapkekes@users.noreply.github.com>
2022-12-28 18:35:01 +0700

    Update dependencies and plugins

--- Task_1_3_1/build.gradle.kts
+++ Task_1_3_1/build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("java")
+    kotlin("jvm") version "1.8.0-Beta"
     id("jacoco")
-    kotlin("jvm") version "1.7.21"
+    id("org.jetbrains.dokka") version "1.7.20"
 }
 
 group = "io.github.kapkekes"
@@ -14,8 +14,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
 }
 
