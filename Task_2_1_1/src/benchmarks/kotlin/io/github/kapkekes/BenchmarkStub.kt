3ec85b7bf036860e4cc27161d9a820cc129e032a
kapkekes <86285206+kapkekes@users.noreply.github.com>
2023-02-05 16:16:29 +0700

    Add BenchmarkStub abstract class

---
+++ Task_2_1_1/src/benchmarks/kotlin/io/github/kapkekes/BenchmarkStub.kt
@@ -0,0 +1,34 @@
+package io.github.kapkekes
+
+import io.github.kapkekes.sieves.SimpleSieve
+import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.Param
+import kotlinx.benchmark.Scope
+import kotlinx.benchmark.Setup
+import kotlinx.benchmark.State
+import java.io.File
+
+/**
+ * The base class for benchmarking functions from [Solution].
+ *
+ * @property sieve The sieve, which will be used for benchmarking; [SimpleSieve] by default.
+ * @property sample The [Collection] of [Int], which will be used. Automatically loads by [loadSample].
+ * @property samplePath The path to the file, which contains the sample for benchmarking.
+ */
+@State(Scope.Benchmark)
+abstract class BenchmarkStub {
+    protected val sieve: Sieve = SimpleSieve().initialize()
+    protected lateinit var sample: Collection<Int>
+
+    @Param("example.txt")
+    var samplePath: String = "example.txt"
+
+    /** Load sample to the [sample] property. */
+    @Setup
+    fun loadSample() {
+        sample = File(samplePath).readLines().map { it.toInt() }
+    }
+
+    /** Actual [Benchmark] routine, which should be overriden. */
+    abstract fun routine(): Boolean
+}
