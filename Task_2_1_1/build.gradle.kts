adfa150e8306c4fdaddf6c3938a91e5a7b4adbe2
kapkekes <86285206+kapkekes@users.noreply.github.com>
2023-02-05 16:15:40 +0700

    Add kotlinx.benchmark to dependencies and configure it

--- Task_2_1_1/build.gradle.kts
+++ Task_2_1_1/build.gradle.kts
@@ -1,21 +1,34 @@
+import kotlinx.benchmark.gradle.JvmBenchmarkTarget
+import org.jetbrains.kotlin.allopen.gradle.AllOpenExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0-Beta"
+    java
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.allopen") version "1.7.20"
     id("jacoco")
     id("org.jetbrains.dokka") version "1.7.20"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.4"
 }
 
 group = "io.github.kapkekes"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
+val benchmarks = "benchmarks"
+sourceSets {
+    create(benchmarks)
+}
+val benchmarksImplementation by configurations
+
 dependencies {
-    testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("test"))
+    benchmarksImplementation(project(mapOf("path" to ":")))
+    benchmarksImplementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.4")
 }
 
 tasks.getByName<Test>("test") {
@@ -28,12 +41,44 @@ tasks.jacocoTestReport {
     }
 }
 
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+configure<AllOpenExtension> {
+    annotation("org.openjdk.jmh.annotations.State")
 }
 
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+benchmark {
+    targets {
+        register(benchmarks) {
+            this as JvmBenchmarkTarget
+            jmhVersion = "1.21"
+        }
+    }
+
+    val samplePaths: Array<String> = File("./src/$benchmarks/resources/").walk().filter {
+        it.isFile
+    }.map {
+        it.path
+    }.toList().toTypedArray()
+    val threadQuantities: Array<Int> = Array(8) { it + 1 }
+
+    configurations {
+        named("main") {
+            mode = "avgt"
+            reportFormat = "csv"
+
+            warmups = 5
+            iterations = 10
+            iterationTime = 5
+            iterationTimeUnit = "sec"
+            outputTimeUnit = "MICROSECONDS"
+
+            param("samplePath", *samplePaths)
+            param("threadQuantity", *threadQuantities)
+        }
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
