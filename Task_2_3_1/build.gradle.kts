d61606815187c2dd3a61f9f03815aed3a7a54c4a
kapkekes <86285206+kapkekes@users.noreply.github.com>
2023-03-03 20:30:41 +0700

    Init module

---
+++ Task_2_3_1/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    id("jacoco")
+    id("org.jetbrains.dokka") version "1.7.20"
+    id("org.openjfx.javafxplugin") version "0.0.13"
+}
+
+javafx {
+    version = "19"
+    modules = listOf("javafx.controls", "javafx.graphics")
+}
+
+group = "io.github.kapkekes"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("no.tornado:tornadofx:1.7.20")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
